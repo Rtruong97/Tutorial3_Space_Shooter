96ba3a64cf2b85026dc1dddf6700d4afdcd29c4b
Rtruong97 <43153493+Rtruong97@users.noreply.github.com>
2018-10-30 03:17:36 -0400

    update

---
+++ Assets/Scripts/GameController.cs
@@ -0,0 +1,53 @@
+﻿using UnityEngine;
+using System.Collections;
+
+
+public class GameController : MonoBehaviour
+{
+    public GameObject hazard;
+    public Vector3 spawnValues;
+    public int hazardCount;
+    public float spawnWait;
+    public float startWait;
+    public float waveWait;
+
+    public GUIText scoreText;
+    private int score;
+
+    void Start()
+    {
+        StartCoroutine (SpawnWaves ());
+
+    }
+
+   IEnumerator SpawnWaves ()
+
+    {
+        yield return new WaitForSeconds(startWait);
+        while (true)
+        {
+            for (int i = 0; i < hazardCount; i++)
+            {
+                Vector3 spawnPosition = new Vector3(Random.Range(-spawnValues.x, spawnValues.x), spawnValues.y, spawnValues.z);
+                Quaternion spawnRotaion = Quaternion.identity;
+                Instantiate(hazard, spawnPosition, spawnRotaion);
+                yield return new WaitForSeconds(spawnWait);
+
+            }
+            yield return new WaitForSeconds (waveWait);
+        }
+    }
+    public void AddScore (int newScoreValue)
+    {
+        score += newScoreValue;
+        UpdateScore();
+
+    }
+
+    private void UpdateScore()
+    {
+        scoreText.text = "Score:" + score;
+
+    }
+
+}
